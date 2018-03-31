238f46169de0fe572a3f8dc21c6af2437f33312a
MrGlode <chouvdj@gmail.com>
2018-04-01 00:41:51 +0200

    Fix item load json

--- item.js
+++ item.js
@@ -1,12 +1,12 @@
 
-var Item = function(id, posX, posY,itemSize,type,imageItem) {
-    //this.type 
-    this.id = id;
+var Item = function(id, posX, posY,itemSize,itemType,imageItem) {
+    this.typeItm; 
+    this.idItm = id;
     this.posX = posX;
     this.posY = posY;
     this.isBusy = false;
-    this.color;
-    this.size = itemSize;
+    this.colorItem;
+    this.sizeItm = itemSize;
     var img = imageItem;
 
 
@@ -15,21 +15,30 @@ var Item = function(id, posX, posY,itemSize,type,imageItem) {
     }
 
     this.draw = function(){
-        //if(type == null) type = this.loadItemType(type);
-        /*
-        stroke(255);
-        fill(this.color)
-        rect(this.posX, this.posY, this.size, this.size);
-        */
-       dWidth = img.width/7;
-       dHeight = img.height /5;
-       image(img,this.posX, this.posY,[sWidth=this.size],[sHeight=this.size],[dx=0],[dy=0],[dWidth],[dHeight])
+        this.typeItm = this.loadItemType(itemType);
+        
+        if(this.typeItm != null){
+            stroke(255);
+            fill(this.colorItm)
+            rect(this.posX, this.posY, this.sizeItm, this.sizeItm);
+            
+            dWidth = img.width/7;
+            dHeight = img.height /5;
+            image(img,this.posX, this.posY,[sWidth=this.sizeItm],[sHeight=this.sizeItm],[dx=0],[dy=0],[dWidth],[dHeight])
+        }
     }
 
     this.loadItemType = function(type){
-        var itemJson = loadJSON("properties/items.json");
-        
-        this.color = color(itemJson.baseGreenItem["colorR"],itemJson.baseGreenItem["colorG"],itemJson.baseGreenItem["colorB"]);
-        return itemJson.type;
+        var itemJson;
+        loadJSON("properties/items.json",function(resp){
+            itemJson = resp;
+            console.log(itemJson);
+            console.log(itemJson.baseGreenItem)
+            console.log(itemJson.baseGreenItem.colorR)
+            console.log(itemJson.baseGreenItem["colorR"])
+            
+            this.colorItem = color(itemJson.baseGreenItem["colorR"],itemJson.baseGreenItem["colorG"],itemJson.baseGreenItem["colorB"]);
+            return itemJson.typeItm;
+        });
     }
 };
